e64adba3e62bdc11f94b965c8c3ddb5b3742e40b
Verdy Bangkit <verdybangkit@gmail.com>
2022-03-28 14:41:11 +0700

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.0
+
+The initial release has DartCodeViewer null safety version of [dart_code_viwewer](https://pub.dev/packages/dart_code_viewer/)
---
+++ example/android/app/src/main/kotlin/com/example/dart_code_viewer2_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_code_viewer2_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
